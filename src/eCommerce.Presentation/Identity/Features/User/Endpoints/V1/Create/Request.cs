e91b9ebda821ba32bb24d2a5de241d52ddabe987
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2024-02-23 23:32:05 +0200

    commit

--- src/eCommerce.Presentation/Identity/Features/User/Endpoints/V1/Create/Request.cs
+++ src/eCommerce.Presentation/Identity/Features/User/Endpoints/V1/Create/Request.cs
@@ -1,6 +1,3 @@
 ﻿namespace eCommerce.Presentation.Identity.Features.User.Endpoints.V1.Create;
 
-public record CreateUserRequest
-{
-    public int Id { get; set; }
-}
+public record CreateUserRequest { }
