36e5df84d54fe261de4ac3bbdccae02ca77d73f6
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2024-03-10 21:51:08 +0200

    commit

---
+++ src/eCommerce.Presentation/Features/Inventory/Service/Endpoints/V1/Get/Endpoint.cs
@@ -0,0 +1,16 @@
+﻿using eCommerce.Domain.Constants;
+using eCommerce.Presentation.Features.Inventory.Services.Services;
+
+namespace eCommerce.Presentation.Features.Inventory.Services.Endpoints.V1.Get;
+
+internal sealed class GetServiceEndpoint : Endpoint<GetServiceRequest, Response>
+{
+    public override void Configure()
+    {
+        Get($"{nameof(ModuleName.Inventory)}/{nameof(FeatureName.Service)}/{nameof(Get)}");
+        Permissions(SystemConstants.Security.Inventory.Services.Get);
+    }
+
+    public override async Task HandleAsync(GetServiceRequest req, CancellationToken ct) =>
+        Response = await Resolve<IServiceService>().GetAsync(req, ct);
+}
---
+++ src/eCommerce.Presentation/Features/Inventory/Service/Endpoints/V1/Update/Endpoint.cs
@@ -0,0 +1,17 @@
+﻿using eCommerce.Domain.Constants;
+using eCommerce.Presentation.Features.Inventory.Services.Endpoints.V1.Update;
+using eCommerce.Presentation.Features.Inventory.Services.Services;
+
+namespace eCommerce.Presentation.Features.Inventory.Services.Endpoints.V1.Get;
+
+internal sealed class UpdateServiceEndpoint : Endpoint<UpdateServiceRequest, Response>
+{
+    public override void Configure()
+    {
+        Put($"{nameof(ModuleName.Inventory)}/{nameof(FeatureName.Service)}/{nameof(Update)}");
+        Permissions(SystemConstants.Security.Inventory.Services.Update);
+    }
+
+    public override async Task HandleAsync(UpdateServiceRequest req, CancellationToken ct) =>
+        Response = await Resolve<IServiceService>().UpdateAsync(req, ct);
+}
