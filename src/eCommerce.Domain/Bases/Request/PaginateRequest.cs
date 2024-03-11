d4287cd961e1a0f67fb46f3b91a478e61a9a0b37
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2024-03-11 19:19:27 +0200

    commit

--- src/eCommerce.Domain/Bases/Request/PaginateRequest.cs
+++ src/eCommerce.Domain/Bases/Request/PaginateRequest.cs
@@ -1,6 +1,7 @@
 ﻿namespace eCommerce.Domain.Bases.Request;
 
 public record PaginateRequest(
+    bool IsDeleted = false,
     int Page = 1,
     int Size = 10,
     OrderByDirection OrderByDirection = OrderByDirection.Ascending,
